9cdf8a382164b35af1ccdfbcd7771813d14c080a
DeepZaym <remember.nadhif@gmail.com>
2024-03-12 09:24:00 +0700

    commit

--- 1.php
+++ 1.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="style1.css">
     <title>Kuliner lain</title>
-    <link rel="stylesheet" href="style1.css">
+    <link rel="stylesheet" href="Model/style1.css">
     
 </head>
 <body>
@@ -13,7 +13,7 @@
         <article class="card">
             <div class="card-img hover"></div>
             <a href="#">
-                <div class="card-img-1" style="background-image: url(rendang.jpg);">
+                <div class="card-img-1" style="background-image: url(Gambar/rendang.jpg);">
                 </div>
             </a>
 
@@ -28,39 +28,43 @@
 
         </article>
 
+
+
         <article class="card">
-            <div class="card-img hover"></div>
+        <div class="card-img hover"></div>
             <a href="#">
-                <div class="card-img-1" style="background-image: url(ketoprak.jpg);">
+                <div class="card-img-1" style="background-image: url(Gambar/Ketoprak.jpg);">
                 </div>
             </a>
 
             <div class="card-info">
-                <span class="card-category">Jawa Barat</span>
+                <span class="card-category">Jakarta</span>
                 <h3 class="card-title">Kuliner Ketoprak</h3>
                 <span class="card-by">
                     apa itu 
-                    <a href='https://www.bing.com/ck/a?!&&p=7ccd585b82855f6dJmltdHM9MTcwMTEyOTYwMCZpZ3VpZD0yYjc3NTc0Zi1lOTkxLTZkYjMtMWYwZC00NGNkZThjNzZjY2UmaW5zaWQ9NTIyMg&ptn=3&ver=2&hsh=3&fclid=2b77574f-e991-6db3-1f0d-44cde8c76cce&psq=ketoprak&u=a1aHR0cHM6Ly9pZC53aWtpcGVkaWEub3JnL3dpa2kvS2V0b3ByYWtfKG1ha2FuYW4p&ntb=1' class="card-admin">Ketoprak???</a>
+                   <a href='https://www.bing.com/ck/a?!&&p=7ccd585b82855f6dJmltdHM9MTcwMTEyOTYwMCZpZ3VpZD0yYjc3NTc0Zi1lOTkxLTZkYjMtMWYwZC00NGNkZThjNzZjY2UmaW5zaWQ9NTIyMg&ptn=3&ver=2&hsh=3&fclid=2b77574f-e991-6db3-1f0d-44cde8c76cce&psq=ketoprak&u=a1aHR0cHM6Ly9pZC53aWtpcGVkaWEub3JnL3dpa2kvS2V0b3ByYWtfKG1ha2FuYW4p&ntb=1' class="card-admin">Ketoprak???</a>
                 </span>
             </div>
+
 
         </article>
 
         <article class="card">
             <div class="card-img hover"></div>
             <a href="#">
-                <div class="card-img-1" style="background-image: url(soto\ banjar.jpg);">
+                <div class="card-img-1" style="background-image: url(Gambar/Sotobanjar.jpg);">
                 </div>
             </a>
 
             <div class="card-info">
-                <span class="card-category">Kalimantan</span>
+                <span class="card-category">Kalimantan Selatan</span>
                 <h3 class="card-title">Kuliner Soto Banjar</h3>
                 <span class="card-by">
                     apa itu 
-                    <a href='https://www.bing.com/ck/a?!&&p=664059458a3dc813JmltdHM9MTcwMTEyOTYwMCZpZ3VpZD0yYjc3NTc0Zi1lOTkxLTZkYjMtMWYwZC00NGNkZThjNzZjY2UmaW5zaWQ9NTI0NQ&ptn=3&ver=2&hsh=3&fclid=2b77574f-e991-6db3-1f0d-44cde8c76cce&psq=soto+banjar&u=a1aHR0cHM6Ly9mb29kLmRldGlrLmNvbS9pbmZvLWt1bGluZXIvZC01ODI1MTcwL3NvdG8tYmFuamFyLWt1bGluZXItaWtvbmlrLWthbGltYW50YW4tc2VsYXRhbi15YW5nLWJlcmt1YWgtZ3VyaWgtY3JlYW15&ntb=1' class="card-admin">Soto Banjar???</a>
+                    <a href='https://www.bing.com/ck/a?!&&p=2538f10b08579bfbJmltdHM9MTcwMTA0MzIwMCZpZ3VpZD0yYjc3NTc0Zi1lOTkxLTZkYjMtMWYwZC00NGNkZThjNzZjY2UmaW5zaWQ9NTg1OA&ptn=3&ver=2&hsh=3&fclid=2b77574f-e991-6db3-1f0d-44cde8c76cce&psq=rendang&u=a1aHR0cHM6Ly93d3cudml2YS5jby5pZC9nYXlhLWhpZHVwL2t1bGluZXIvMTE4NTg1OC1qYWRpLW1ha2FuYW4tdGVybGV6YXQtZHVuaWEtcmVuZGFuZy1zdWRhaC1kaWFrdWktdW5lc2NvLWJlbHVtLXNpaA&ntb=1' class="card-admin">Rendang???</a>
                 </span>
             </div>
+
 
         </article>
 
